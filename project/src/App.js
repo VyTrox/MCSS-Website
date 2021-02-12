0010f9f336a40c779bf67ec14d624f53c6975b4f
congyoua <77039291+congyoua@users.noreply.github.com>
2021-02-12 03:58:50 -0500

    [P301-5] Add page info to App.js

--- project/src/App.js
+++ project/src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Main from './Main';
 import AboutUs from './Componet/AboutUs/aboutUs';
+import Programs from './Componet/Programs/Requirements';
 
 
 class App extends React.Component{
@@ -15,6 +16,7 @@ class App extends React.Component{
           <Switch>
             <Route exact path='/' render={() => <Main></Main>}/>
             <Route exact path='/AboutUs' render={() => <AboutUs></AboutUs>}/>
+			      <Route exact path='/Programs' render={() => <Programs></Programs>}/>
           </Switch>
         </BrowserRouter>
       </div>
